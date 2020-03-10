6d21b962387c697f1666b2a1c9ddaa00f498f7e5
Andre Skeene <halodre24@gmail.com>
2020-03-10 18:28:57 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,7 @@
 class Transfer
 
-  def initialize(name_1, name_2, amount)
-    @name_1 = name_1
+  def initialize(sender, name_2, amount)
+    @sender = sender
     @name_2 = name_2
     @amount = amount
   end
