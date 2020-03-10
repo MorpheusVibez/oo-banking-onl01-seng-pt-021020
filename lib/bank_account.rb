521e3368f22513b06a837871beafa2fb4bbffa94
Andre Skeene <halodre24@gmail.com>
2020-03-10 18:25:06 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,6 +1,6 @@
 class BankAccount
-  attr_accessor :status
-  attr_reader :name, :balance
+  attr_accessor :status, :balance
+  attr_reader :name
 
   def initialize(name)
     @name = name
@@ -8,9 +8,6 @@ class BankAccount
     @status = "open"
   end
 
-  def balance
-    @balance
-  end
   def deposit(money)
     @balance += money
   end
