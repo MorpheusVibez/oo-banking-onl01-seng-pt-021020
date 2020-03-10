758bbdfd36410b4f2fee372e45c39139b5879578
Andre Skeene <halodre24@gmail.com>
2020-03-10 18:48:11 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,8 +22,13 @@ class Transfer
   end
 
   def execute_transaction
-    sender.balance -= @amount
-    receiver.balance += @amount
-    Transfer.status = "complete"
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
   end
 end
