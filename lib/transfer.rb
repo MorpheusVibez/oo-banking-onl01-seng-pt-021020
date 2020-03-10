651ad73026490a82202ef8fff83337599b550f72
Andre Skeene <halodre24@gmail.com>
2020-03-10 19:03:26 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,12 +26,8 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    end
-
-    if sender.valid? == false
-      @status = rejected
+    else
       "Transaction rejected. Please check your account balance."
-    end
   end
 
   def reverse_transfer
