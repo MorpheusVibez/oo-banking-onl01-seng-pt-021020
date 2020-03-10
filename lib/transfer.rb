f49da744268c34a7d9e59d8b4c67c746fc651af5
Andre Skeene <halodre24@gmail.com>
2020-03-10 18:44:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,8 +18,8 @@ class Transfer
   end
 
   def execute_transaction
-    # sender.balance -= @amount
-    # receiver.balance += @amount
+    sender.balance -= @amount
+    receiver.balance += @amount
     # status = "complete"
   end
 end
